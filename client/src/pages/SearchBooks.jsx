186a11f50981f384e69c03aaeac582225fd22c6b
Christopher <christopher.ferraro34@gmail.com>
2024-03-25 16:05:40 -0400

    Update SearchBooks.jsx

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -12,6 +12,11 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+import { SAVE_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
+const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
